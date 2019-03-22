61bc0b968d2301208f963a1fc1f1c4a88ac24673
Andre Kurait <AndreKurait@gmail.com>
2019-03-22 17:33:17 -0500

    kmeans

--- Python/kmeans.py
+++ Python/kmeans.py
@@ -1,6 +1,67 @@
-class K_Means:
+import copy
+import numpy as np
+import math
+import random
 
-	def __init__(self, k =3, tolerance = 0.0001, max_iterations = 500):
-		self.k = k
-		self.tolerance = tolerance
-		self.max_iterations = max_iterations
+class K_Means:
+    def __init__(self, xAxis, yAxis, classifications, tol=0.0001, max_iter=500):
+        self.tol = tol
+        self.max_iter = max_iter
+
+        assert len(xAxis) == len(yAxis)
+        self.k = len(set(classifications))
+
+        data = [(xAxis[i], yAxis[i]) for i in range(len(xAxis))]
+        uniqueData = list(set(data))
+        random.shuffle(uniqueData)
+        self.centroids = {}
+        for i in range(self.k):
+            self.centroids[i] = uniqueData[i]
+
+        iterations = 0
+        while (iterations < self.max_iter):
+            iterations += 1
+            distances = []
+            for i in range(len(xAxis)):
+                distances.append([])
+                for j in range(self.k):
+                    centroid = self.centroids[j]
+                    squaredDistance = (float(xAxis[i]) - float(centroid[0])) ** 2 + (float(yAxis[i]) - float(centroid[1])) ** 2
+                    distances[i].append(math.sqrt(squaredDistance))
+
+            self.classifications = {}
+            for i in range(len(xAxis)):
+                self.classifications[i] = distances[i].index(min(distances[i]))
+
+            old_centroids = copy.deepcopy(self.centroids)
+
+            for i in range(self.k):
+                xcentroidPoints = []
+                ycentroidPoints = []
+                for x in range(len(xAxis)):
+                    if(self.classifications[x] == i):
+                        xcentroidPoints.append(xAxis[x])
+                        ycentroidPoints.append(yAxis[x])
+                self.centroids[i] = (np.mean(xcentroidPoints), np.mean(ycentroidPoints))
+
+            if old_centroids == self.centroids:
+                break
+
+            optimized = True
+            for i in range(self.k):
+                original_centroid = old_centroids[i]
+                current_centroid = self.centroids[i]
+                if np.sum((current_centroid[0] - original_centroid[0]) / original_centroid[0]*100.0) > self.tol and np.sum((current_centroid[1] - original_centroid[1]) / original_centroid[1]*100.0) > self.tol:
+                    optimized = False
+
+            if optimized:
+                break
+
+    def predict(self, x, y):
+        distances = []
+        for i in range(self.k):
+            centroid = self.centroids[i]
+            squaredDistance = (x - float(centroid[0])) ** 2 + (y - float(centroid[1])) ** 2
+            distances.append(np.sqrt(squaredDistance))
+        classification = distances.index(min(distances))
+        return classification
--- Python/test.py
+++ Python/test.py
@@ -1,8 +1,52 @@
-import pandas as pd
-import numpy as np
-import sklearn as sc
-from matplotlib import pyplot as plt
-from sklearn.preprocessing import LabelEncoder
 
-df_shakespeare = pd.read_csv('./winequality-red.csv')
-df_shakespeare.head()
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+import random
+
+from kmeans import K_Means
+
+df_iris = pd.read_csv('../data/Iris.csv')
+df_iris.head()
+
+
+xAxis      = df_iris['PetalLengthCm']
+xAxisLabel = 'Petal Length (cm)'
+yAxis      = df_iris['PetalWidthCm']
+yAxisLabel = 'Petal Width (cm)'
+classVerify = df_iris['Species']
+
+#Feature enginering
+if xAxis.min() == xAxis.max() or yAxis.min() == yAxis.max():
+    raise Exception('Cannot run K-Means without at-least 2 distinct data points')
+xAxis -= xAxis.min()
+xAxis /= xAxis.max()
+xAxis = 4*xAxis - 2
+
+yAxis -= yAxis.min()
+yAxis /= yAxis.max()
+yAxis = 4*yAxis - 2
+
+colmap = [name for name, hex in matplotlib.colors.cnames.items()]
+random.Random(0).shuffle(colmap)
+k = len(set(classVerify))
+print("K is %s" % k)
+kmeans = K_Means(xAxis, yAxis, classVerify)
+
+classificationColors = []
+for i in range(len(xAxis)):
+    index = kmeans.predict(xAxis[i],yAxis[i])
+    color = colmap[index]
+    classificationColors.append(color)
+
+fig = plt.figure(figsize=(5, 5))
+
+plt.scatter(xAxis, yAxis, color=classificationColors, edgecolor='k')
+print(kmeans.centroids)
+plt.scatter([kmeans.centroids[i][0] for i in range(k)], [kmeans.centroids[i][1] for i in range(k)], color='k')
+plt.xlim(-2.5, 2.5)
+plt.ylim(-2.5, 2.5)
+plt.show()
